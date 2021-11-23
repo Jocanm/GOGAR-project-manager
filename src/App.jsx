acf04826f17323f7aebb8228a1b5c8bc462a830e
Jose Angarita <amjose09@gmail.com>
2021-11-23 13:08:25 -0500

    Apollo cliente configurado

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import PublicLayout from './layout/PublicLayout'
 import Index from './pages/Index'
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client'
 import './App.css'
+
+const httpLink = createHttpLink({
+    uri:"http://localhost:4000/graphql"
+})
+
+const client = new ApolloClient({
+    uri:httpLink,
+    cache: new InMemoryCache()
+})
 
 const App = () => {
     return (
-        <>
+        <ApolloProvider cliente={client}>
             <PublicLayout>
                 <Index/>
             </PublicLayout>
-        </>
+        </ApolloProvider>
     )
 }
 
