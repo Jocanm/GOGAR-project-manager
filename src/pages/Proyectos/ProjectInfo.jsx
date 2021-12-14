d905bf42b683c82186bb84f494136299ac41be58
Jose Angarita <amjose09@gmail.com>
2021-12-13 22:45:18 -0500

    Error

--- src/pages/Proyectos/ProjectInfo.jsx
+++ src/pages/Proyectos/ProjectInfo.jsx
@@ -293,9 +293,7 @@ const Avances = ({ _id }) => {
 
 const AvanceItem = ({e,refetch}) => {
 
-    const [nueva, setNueva] = useState("")
-    const [open, setOpen] = useState(false)
-    const [observaciones, setObservaciones] = useState(false)
+
 
     const [agregarObs, { data, loading }] = useMutation(AGREGAR_OBSERVACIONES)
 
